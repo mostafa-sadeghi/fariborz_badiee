94e7a6bb3dcc8143defbf75e0ad744f28c1b391e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-01 22:59:08 +0330

    10azar

--- first.py
+++ first.py
@@ -1,1 +1,57 @@
-print('hi')
+# data type
+# numbers bool str
+
+# numbers:  int float complex
+
+# x = 1
+# print(x)
+# print(type(x))
+# y = 1.0
+# print(type(y))
+
+# z = 'a'
+# print(type(z))
+
+# number1 = 2
+# number2 = 3
+
+# res = number1 + number2
+# print(res)
+# res = number1 - number2
+# print(res)
+# res = number1 * number2
+# print(res)
+# res = number1 / number2
+# print(res)
+# res = number1 // number2
+# print(res)
+# res = number1 ** number2
+# print(res)
+
+# message1 = 'a'
+# message2 = 'aaaaaa'
+# message3 = ""
+# print(message1)
+
+# x = 20 + 10 * 3
+# print(x)
+# x = (20 + 10) * 3
+# print(x)
+
+# z = 1 + 2j
+# print(type(z))
+
+
+name = 'ali'
+family = 'rezaei'
+
+message = name + ' ' + family
+print(message)
+
+# name = 'ali' + 2 -> error
+message = '*' * 3
+print(message)
+message = 3 * '*'
+print(message)
+
+print('aaa'*3)
